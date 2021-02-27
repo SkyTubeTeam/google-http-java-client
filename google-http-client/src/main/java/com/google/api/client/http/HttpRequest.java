3b9c80e6d9d5162c0f80f2a1f4331046c4188b40
Zsombor Gegesy <gzsombor@gmail.com>
2021-02-27 23:22:19 +0100

    Set version to 1.39.0.1

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -53,7 +53,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = getVersion();
+  public static final String VERSION = "1.39.0";
 
   /**
    * User agent suffix for all requests.
